0ce799f05624d86d3387295732de062dcf1a8850
Dane Watts <danewatts97@gmail.com>
2019-04-01 14:12:14 +0100

    Answers: Added activity 2 answers ' '

--- src/main/scala/controllers/stockprice/StockPriceController.scala
+++ src/main/scala/controllers/stockprice/StockPriceController.scala
@@ -12,9 +12,28 @@ trait StockPriceController {
   val companyID: String = "companyID1"
 
   // What happens when the future fails? Can we have different behaviour for different errors?
-  def getStockPrice: Future[String] = ???
+  def getStockPrice: Future[String] = {
+    connector.getCompany(companyID).flatMap {
+      company =>
+        connector.getStockPrice(company).map {
+          price =>
+            s"Company: ${company.name}; Stock price: £$price"
+        }
+    } recover {
+      case _: CompanyNotFoundException => "Company not found"
+      case _: Exception => "An error has occurred"
+    }
+  }
 
-  def getStockPriceWithFor: Future[String] = ???
+  def getStockPriceWithFor: Future[String] = {
+    for {
+      company <- connector.getCompany(companyID)
+      price <- connector.getStockPrice(company)
+    } yield s"Company: ${company.name}; Stock price: £$price"
+  } recover {
+    case _: CompanyNotFoundException => "Company not found"
+    case _: Exception => "An error has occurred"
+  }
 
 }
 
