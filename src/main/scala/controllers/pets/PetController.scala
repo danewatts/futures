9cacdd7196fe88e53107ae32039b00b4c464861c
Dane Watts <danewatts97@gmail.com>
2019-04-03 15:39:03 +0100

    Answers: Added answers for activity 3

--- src/main/scala/controllers/pets/PetController.scala
+++ src/main/scala/controllers/pets/PetController.scala
@@ -1,18 +1,46 @@
 package controllers.pets
 
-import connectors.pets.PetConnector
+import connectors.pets.{PetConnector, PetNotFoundException}
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait PetController {
 
   val connector: PetConnector
   val applyDiscount: Boolean
 
-  //TODO Implement using map and flatMap
-  def getPriceOfPet(id: String): Future[Double] = ???
+  def getPriceOfPet(id: String): Future[Double] = {
+    connector.getPet(id).flatMap {
+      pet =>
+        connector.getPrice(pet).map{
+          price =>
+            if(applyDiscount) {
+              price * 0.9
+            } else price
+        }
+    } recover {
+      case _: PetNotFoundException =>
+        0
+      case e: Exception =>
+        throw e
+    }
+  }
 
 
-  def getPriceOfPetWithFor(id: String): Future[Double] = ???
+  def getPriceOfPetWithFor(id: String): Future[Double] = {
+    for {
+      pet <- connector.getPet(id)
+      price <- connector.getPrice(pet)
+    } yield {
+      if(applyDiscount) price * 0.9
+      else price
+    }
+  } recover {
+    case _: PetNotFoundException =>
+      0
+    case e: Exception =>
+      throw e
+  }
 
 }
